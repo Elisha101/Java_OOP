c4b687c2edb24b9e8c504430d023c7f5275a3787
Elisha <ukaigve1990@mail.ru>
2023-07-17 21:20:02 +0300

    dz

---
+++ lesson1/Main.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        Product product1 = new Product();
+        product1.setName("twix");
+
+        System.out.println(product1.getName());
+    }
+}
